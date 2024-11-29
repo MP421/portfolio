f524116b08e3e88843fac6c30310193704b098d7
Jon Owen <jonathandowen1989@gmail.com>
2024-11-29 22:43:33 +0000

    - Created .env file for storing secrets - Added dotenv for using environment variables - Modified server.js so function that handles sending emails operates asynchrously

--- server/server.js
+++ server/server.js
@@ -2,41 +2,46 @@ import express from 'express';
 import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/send-email', (req, res) => {
-  const { name, email, message } = req.body;
-
+const sendEmail = async (name, email, message) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'fullstackspecialisttoday@gmail.com',
-      pass: 'joshponijvcmlmim'
-    }
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
   });
 
   const mailOptions = {
     from: email,
-    to: 'fullstackspecialisttoday@gmail.com',
+    to: process.env.GMAIL_USER,
     subject: `Message from ${name}`,
-    text: message
+    text: message,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error occurred:', error);
-      return res.status(500).send(error.toString());
-    }
+  return await transporter.sendMail(mailOptions);
+};
+
+app.post('/api/send-email', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    const info = await sendEmail(name, email, message);
     console.log('Email sent successfully:', info.response);
     res.status(200).send('Email sent: ' + info.response);
-  });
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    res.status(500).send('Failed to send email: ' + error.message);
+  }
 });
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
-
-export default app;
